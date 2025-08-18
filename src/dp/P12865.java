9611611d0bbcf1383e1b43e1492bf3088e9c6ff8
MINYONG PARK <parkminyong22@gmail.com>
2025-08-18 21:27:32 +0900

    하노이탑 수정

--- src/dp/P12865.java
+++ src/dp/P12865.java
@@ -7,17 +7,20 @@ public class P12865 {
         Scanner sc = new Scanner(System.in);
         int N = sc.nextInt();
         int K = sc.nextInt();
-        int[] dp = new int[K+1];
+        int[][] items = new int[N][2];
+        int[][] dp = new int[N][2];
 
         for (int i = 0; i < N; i++) {
             int weight = sc.nextInt();
             int value = sc.nextInt();
 
             for (int j = K; j >=weight; j--) {
-                dp[j] = Math.max(dp[j], dp[j-weight] + value);
+//                dp[j] = Math.max(dp[j], dp[j-weight] + value);
             }
         }
 
         System.out.println(dp[K]);
+        // dp(n) = Max(dp(n-1),  dp(n) + dp(n-1))
+
     }
 }
