87cd9097e63e88c0a93e29eacbc5eeb5b2f375b5
MINYONG PARK <parkminyong22@gmail.com>
2025-10-08 19:50:03 +0900

    Update P28107.java

--- src/datastructure/queue/P28107.java
+++ src/datastructure/queue/P28107.java
@@ -8,36 +8,42 @@ public class P28107 {
         int N = sc.nextInt();
         int M = sc.nextInt();
 
-        List<Set<Integer>> orders = new ArrayList<>();
+        // 이미 초밥을 먹었는지 확인
+        Set<Integer>[] eaten = new HashSet[N];
         for (int i = 0; i < N; i++) {
-            int k = sc.nextInt();
-            Set<Integer> set = new HashSet<>();
-            for (int j = 0; j < k; j++) {
-                set.add(sc.nextInt());
-            }
-            orders.add(set);
+            eaten[i] = new HashSet<>();
         }
 
-        int[] eaten = new int[N];
-        Set<Integer>[] already = new HashSet[N];
+        // 스시번호당 먹고 싶은 사람 추가
+        Map<Integer, List<Integer>> sushiOrderMap = new HashMap<>();
         for (int i = 0; i < N; i++) {
-            already[i] = new HashSet<>();
+            int k = sc.nextInt();
+            for (int j = 0; j < k; j++) {
+                int sushi = sc.nextInt();
+                sushiOrderMap.putIfAbsent(sushi, new ArrayList<>());
+                sushiOrderMap.get(sushi).add(i);
+            }
         }
+
+        int[] ans = new int[N];
 
         for (int i = 0; i < M; i++) {
             int sushi = sc.nextInt();
 
-            for (int person = 0; person < N; person++) {
-                if (orders.get(person).contains(sushi) && !already[person].contains(sushi)) {
-                    eaten[person]++;
-                    already[person].add(sushi);
+            if (!sushiOrderMap.containsKey(sushi)) continue; // 주문 목록에 없는 스시는 버린다
+
+            List<Integer> customers = sushiOrderMap.get(sushi); // 해당 스시를 주문한 사람들
+            for (int customer : customers) {
+                if (!eaten[customer].contains(sushi)) { // 이미 먹었는지 확인
+                    eaten[customer].add(sushi);
+                    ans[customer]++;
                     break;
                 }
             }
         }
 
         for (int i = 0; i < N; i++) {
-            System.out.print(eaten[i] + " ");
+            System.out.print(ans[i] + " ");
         }
     }
 }
